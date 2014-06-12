faf4092b62a0df8c5efa007155e5cbe1fa46598f
Suleyman Calik <suleymancalik@gmail.com>
2014-06-13 02:22:57 +0300

    iOSDB.swift file created

---
+++ SWIFT/Example/iOSDB/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  iOSDB
+//
+//  Created by Suleyman Calik on 12.06.2014.
+//  Copyright (c) 2014 Suleyman Calik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+                            
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+     }
+
+
+
+}
+
---
+++ SWIFT/iOSDB.swift
@@ -0,0 +1,10 @@
+//
+//  iOSDB.swift
+//  iOSDB
+//
+//  Created by Suleyman Calik on 13.06.2014.
+//  Copyright (c) 2014 Suleyman Calik. All rights reserved.
+//
+
+import Foundation
+
