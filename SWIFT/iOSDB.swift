2d6c33c1252942ba8fe32cdbf3598b4786e5825a
Suleyman Calik <suleymancalik@gmail.com>
2014-06-29 16:59:25 +0300

    sqlite3 framework added to project

--- SWIFT/iOSDB.swift
+++ SWIFT/iOSDB.swift
@@ -8,3 +8,43 @@
 
 import Foundation
 
+
+
+
+/*
++(void)setupWithFileName:(NSString *)name
+extension:(NSString *)extension
+    version:(NSString *)version;
+
+/**
+Supports simple select queries like:
+SELECT a , b FROM todos WHERE a = 1 AND b = 2
+
+If elements argument is nil or empty array:
+SELECT * FROM todos WHERE a = 1 AND b = 2
+*/
++(NSArray *)selectFromTable:(NSString *)table
+elements:(NSArray *)elements
+keys:(NSDictionary *)keys;
+
+
++(BOOL)insertToTable:(NSString *)tableName
+elements:(NSDictionary *)elements;
+
+
++(BOOL)updateTable:(NSString *)tableName
+withControlKey:(NSDictionary *)controlKey
+andElements:(NSDictionary *)elements;
+
+
++(BOOL)deleteFromTable:(NSString *)table
+withControlKey:(NSString *)key
+andValue:(NSString *)value;
+
++(void)clearTable:(NSString *)table;
+
++(void)clearTables:(NSArray *)tables;
+
++(void)clearAllTables;
+
+*/
